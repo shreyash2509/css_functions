54914212b37f7377284f3df7e52c7edf55b1ca0f
Shreyash02509 <shreyash02509@gmail.com>
2022-02-17 21:52:03 +0530

    first_commit

---
+++ src/main.js
@@ -0,0 +1,87 @@
+function margin1() {  
+  document.getElementsByClassName("text")[0].style.margin="100px 0px 0px 0px";
+}
+function margin2() {  
+  document.getElementsByClassName("text")[0].style.margin="0px 100px 0px 0px";
+}
+function margin3() {  
+  document.getElementsByClassName("text")[0].style.margin="0px 0px 100px 0px";
+}
+function margin4() {  
+  document.getElementsByClassName("text")[0].style.margin="0px 0px 0px 100px";
+}
+
+function padding1() {  
+  document.getElementsByClassName("text-p")[0].style.padding="100px 0px 0px 0px";
+}
+function padding2() {  
+  document.getElementsByClassName("text-p")[0].style.padding="0px 100px 0px 0px";
+}
+function padding3() {  
+  document.getElementsByClassName("text-p")[0].style.padding="0px 0px 100px 0px";
+}
+function padding4() {  
+  document.getElementsByClassName("text-p")[0].style.padding="0px 0px 0px 100px";
+}
+
+
+
+function display1() {  
+  document.getElementsByClassName("text_dis")[0].style.display="inline";
+}
+function display2() {  
+  document.getElementsByClassName("text_dis")[0].style.display="block";
+}
+function display3() {  
+  document.getElementsByClassName("text_dis")[0].style.display="contents";
+}
+function display4() {  
+  document.getElementsByClassName("text_dis")[0].style.display="flex";
+}
+function display5() {  
+  document.getElementsByClassName("text_dis")[0].style.display="grid";
+}
+function display6() {  
+  document.getElementsByClassName("text_dis")[0].style.display="inline-block";
+}
+function display7() {  
+  document.getElementsByClassName("text_dis")[0].style.display="inline-flex";
+}
+function display8() {  
+  document.getElementsByClassName("text_dis")[0].style.display="inline-grid";
+}
+function display9() {  
+  document.getElementsByClassName("text_dis")[0].style.display="table";
+}
+
+
+
+
+function position1() {  
+  document.getElementsByClassName("pos")[0].style.position="static";
+  document.getElementsByClassName("pos")[0].style.bottom="auto";
+  document.getElementsByClassName("pos")[0].style.left="auto";
+}
+function position2() {  
+  document.getElementsByClassName("pos")[0].style.position="relative";
+  document.getElementsByClassName("pos")[0].style.left="30px";
+  
+}
+function position3() {  
+  document.getElementsByClassName("pos")[0].style.position="fixed";
+  document.getElementsByClassName("pos")[0].style.bottom="35vh";
+  document.getElementsByClassName("pos")[0].style. width="300px";
+  document.getElementsByClassName("pos")[0].style.left="auto";
+  
+}
+function position4() {  
+  document.getElementsByClassName("pos")[0].style.position="absolute";
+  document.getElementsByClassName("pos")[0].style.bottom="auto";
+  document.getElementsByClassName("pos")[0].style. width="auto";
+  document.getElementsByClassName("pos")[0].style.left="auto";
+}
+function position5() {  
+  document.getElementsByClassName("pos")[0].style.position="sticky";
+}
+
+
